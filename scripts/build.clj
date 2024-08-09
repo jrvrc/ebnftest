8a545e0613b8741939426767ad6d690e873c6723
jrvrc <8863835+jrvrc@users.noreply.github.com>
2024-08-09 13:19:41 +0200

    Update build.clj

--- scripts/build.clj
+++ scripts/build.clj
@@ -5,8 +5,8 @@
 (let [start (System/nanoTime)]
   (b/build "src"
     {:main 'ebnftest.core
-     :output-to "out/ebnftest.js"
-     :output-dir "out"
+     :output-to "_site/ebnftest.js"
+     :output-dir "_site"
      :verbose true})
   (println "... done. Elapsed" (/ (- (System/nanoTime) start) 1e9) "seconds"))
 
